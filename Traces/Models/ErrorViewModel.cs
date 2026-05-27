1f94ed67a2eebf8fbdb2e6156c1339cbed103981
Camelia Ciocan <camelia-carmen.ciocan@oebb.at>
2026-05-27 13:08:59 +0200

    Projektdateien hinzufügen.

---
+++ Traces/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Traces.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
