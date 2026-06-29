7b1715f86223775e5d05730eec408d8f549e0161
Camelia Ciocan <camelia-carmen.ciocan@oebb.at>
2026-06-29 14:54:23 +0200

    prep pdf implement

--- Traces/Services/ITripService.cs
+++ Traces/Services/ITripService.cs
@@ -56,6 +56,8 @@ namespace Traces.Services
         Task DeleteTripAsync(int tripId);
         Task RemoveTripMemberAsync(int tripId, int memberId);
         Task UpdateRouteTravelModeAsync(int fromActivityId, int toActivityId, string travelMode);
+        Task GeneratePdfAsync();
+        Task DownloadPdfAsync();
     }
 
     public class ReorderTimelineItemDto
---
+++ Traces/Services/PdfService.cs
@@ -0,0 +1,12 @@
+﻿using QuestPDF.Infrastructure;
+
+namespace Traces.Services
+{
+    public class PdfService
+    {
+        public PdfService()
+        {
+            QuestPDF.Settings.License = LicenseType.Community;
+        }
+    }
+}
